2b214503404e61f20c8baab70b72bddd48039a72
DESKTOP-GCH0UL8\박상현 <dsfsf@d.com>
2021-01-22 01:33:30 +0900

    기본 모델 추가

--- no_reporting_penalty/models.py
+++ no_reporting_penalty/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class PenaltyTaxRate(models.Model):
+    """가산세 기준(부정행위/일반)
+    """
+
+    title = models.CharField(max_length=30)  #가산세 항목 description 
+    rate = models.FloatField() #가산세 항목별 세율 ex) 40%
+    updated_at = models.DateTimeField() #데이터 업데이트 시각
+
+
+class PenaltyDiscountRate(models.Model):
+    """감면 기준(1개월이내/1개월초과... 등등)
+    """
+
+    title = models.CharField(max_length=30)  #가산세 감면 기간 description ex) 가. 1개월 이내
+    period = models.IntegerField() #감면 기한 적용 기간 day 기준이므로 int 활용. ex) 30 DAYs
+    rate = models.FloatField() #감면율 ex) 90%
+    updated_at = models.DateTimeField() #데이터 업데이트 시각
+
+
+
+
