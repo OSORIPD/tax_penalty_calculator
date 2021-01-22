54cafafe98dde5318d1c581b9d4438ebe4e6e457
DESKTOP-GCH0UL8\박상현 <dsfsf@d.com>
2021-01-22 23:59:10 +0900

    페이지 테스트

--- no_reporting_penalty/views.py
+++ no_reporting_penalty/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from .models import PenaltyTaxRate
 # Create your views here.
+
+def index(request):
+
+    penaltyTaxRates = PenaltyTaxRate.objects.all() 
+
+    return render(
+        request,
+        'no_reporting_penalty/index.html',
+        {
+            'penaltyTaxRates' : penaltyTaxRates,
+        }
+    )
+
+
+# def single_post_page(request, pk):
+#     penaltyTaxRate = PenaltyTaxRate.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         '/single_post_page.html',
+#         {
+#             'penaltyTaxRate' : penaltyTaxRate,
+#         }
+#     )
