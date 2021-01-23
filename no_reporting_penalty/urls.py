1ff13746130aff67ecd436c6154b066b29dea0b6
osoriproduction <heinekenberg16@gmail.com>
2021-01-23 12:37:45 +0900

    navigation bar 추가

--- no_reporting_penalty/urls.py
+++ no_reporting_penalty/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('your-name/',views.get_name, name='your-name'),
-    path('thanks/', views.index, name='index'),
+    # path('your-name/',views.get_name, name='your-name'),
+    # path('thanks/', views.index, name='index'),
 
-    path('', views.login_home, name='login_home'),
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('no_reporting/', views.no_reporting, name='no_reporting'),
 
-    # path('usd/',views.usd, name='usd'),
-    # path('index/', views.index, name='index'),
 
 ]
 
--- no_reporting_penalty/views.py
+++ no_reporting_penalty/views.py
@@ -14,9 +14,13 @@ from .models import PenaltyTaxRate
 def index(request):
     return render(request,'no_reporting_penalty/index.html', { })
 
+def no_reporting(request):
+    return render(request,'no_reporting_penalty/no_reporting.html', { })
 
-def login_home(request):
-    return render(request,'no_reporting_penalty/login.html', { })
+
+
+# def login_home(request):
+#     return render(request,'no_reporting_penalty/login.html', { })
 
 
 # def usd(request):
@@ -26,49 +30,49 @@ def login_home(request):
 #     return render(request,'no_reporting_penalty/index.html', {'amount':amount2,'usd':usd})
 
 
-from django import forms
-class NameForm(forms.Form):
-	your_name = forms.CharField(label = 'Your Name', max_length=100,widget=forms.Textarea)
+# from django import forms
+# class NameForm(forms.Form):
+# 	your_name = forms.CharField(label = 'Your Name', max_length=100,widget=forms.Textarea)
 
 
 
-from django.http import HttpResponseRedirect
-def get_name(request):
+# from django.http import HttpResponseRedirect
+# def get_name(request):
 
-	#POST방식이면 제출된 폼을 처리
-	if request.method=='POST':
-		form = NameForm(request.POST)
-		if form.is_valid():
-			#폼 데이터가 유효하면 cleaned_data로 복사된다
-			new_name = form.cleaned_data['your_name']
+# 	#POST방식이면 제출된 폼을 처리
+# 	if request.method=='POST':
+# 		form = NameForm(request.POST)
+# 		if form.is_valid():
+# 			#폼 데이터가 유효하면 cleaned_data로 복사된다
+# 			new_name = form.cleaned_data['your_name']
 			
-			#추가적 처리들...(생략)
-			#새로운 URL로 리다이렉션
+# 			#추가적 처리들...(생략)
+# 			#새로운 URL로 리다이렉션
             
-			return HttpResponseRedirect('/thanks/')
-	        #return render(request,'index.html',{'form':form})
+# 			return HttpResponseRedirect('/thanks/')
+# 	        #return render(request,'index.html',{'form':form})
 
-	#POST가 아닌 GET이면 빈폼을 보여준다
-	else:
-		form = NameForm() #아까 정의해둔 폼클래스
+# 	#POST가 아닌 GET이면 빈폼을 보여준다
+# 	else:
+# 		form = NameForm() #아까 정의해둔 폼클래스
 
-	return render(request,'index.html',{'form':form})
+# 	return render(request,'index.html',{'form':form})
 
 
 
-from .forms import LoginForm
+# from .forms import LoginForm
 
-def login(request):
-    if request.method == 'GET':
-        form = LoginForm
-    elif request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            request.session['user'] = form.user_id
-            return redirect('/')
+# def login(request):
+#     if request.method == 'GET':
+#         form = LoginForm
+#     elif request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             request.session['user'] = form.user_id
+#             return redirect('/')
  
  
-    return render(request, 'login.html', {'form': form})
+#     return render(request, 'login.html', {'form': form})
 
 
 
